2e62528a6080a0311c205ff6ad63e529036307f9
Ayham <ayhampt011@gmail.com>
2026-06-08 17:44:39 +0530

    added winston logger

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import fs from "fs/promises";
 import { InternalServerError } from "../utils/errors/app.error";
+import logger from "../config/logger";
 
 export const pingHandler = async (req: Request, res: Response,next:NextFunction) => {
   try {
-    await fs.readFile("sample");
+    logger.info("pong hit");
     res.status(200).json({
       message:"pong"
     })
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { serverConfig } from "./config";
 import v1Router from "./routers/v1/index.router";
 import { genericErrorHandler } from "./middlewares/error.middleware";
+import logger from "./config/logger";
+import { attachCorrelationMiddleware } from "./middlewares/correlationId.middleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.use(attachCorrelationMiddleware);
 /**
  * Registering all the routes and their corresponding routes with out app server object
  */
@@ -15,6 +18,6 @@ app.use("/api/v1", v1Router);
 app.use(genericErrorHandler);
 
 app.listen(serverConfig.PORT, () => {
-  console.log(`Server running on port http://localhost:${serverConfig.PORT}`);
-  console.log(`press CTL+C to stop the Server`);
+  logger.info(`Server running on port http://localhost:${serverConfig.PORT}`);
+  logger.info(`press CTL+C to stop the Server`);
 });
